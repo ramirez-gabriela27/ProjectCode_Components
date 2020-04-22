6d935e5019ffcf06de40ce96b5f8e103039ddab5
anferguson <alfe6135@colorado.edu>
2020-04-21 21:19:28 -0600

    Login PHP

--- RDrawWEB/login.php
+++ RDrawWEB/login.php
@@ -1,4 +1,5 @@
 <?php
+$function = $_POST['function'];
   $connect = mysql_connect("user_info","postgres","rdraw");if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db("user_info", $connect);
     $user_info = “INSERT INTO table_name (username, email) VALUES ('$_POST[username]', '$_POST[email]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
 ?>
