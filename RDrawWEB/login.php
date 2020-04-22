79b1346f347633e99883333ec6369d233a61c6a7
ramirez-gabriela27 <tolosaramirez.gabriela12@hotmail.com>
2020-04-21 21:32:06 -0600

    PHP File edits | Alex

--- RDrawWEB/login.php
+++ RDrawWEB/login.php
@@ -1,5 +1,4 @@
 <?php
-$function = $_POST['function'];
-  $connect = mysql_connect("user_info","postgres","rdraw");if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db("user_info", $connect);
-    $user_info = “INSERT INTO table_name (username, email) VALUES ('$_POST[username]', '$_POST[email]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
-?>
+$dbconn = pg_connect("host=localhost dbname=user_info user=ohhcfvbrbxqwyk password=rdraw")
+    or die('Could not connect: ' . pg_last_error());
+  ?>
